7d8299e532de947f7602cf2faeb29f7675a4ddef
Eunsoo Lee <eunsoogi.lee@gmail.com>
2024-10-28 13:17:59 +0900

    fix: invalid config

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack5: true,
-  webpack: (config, options) => {
-    config.cache = false;
-    return config;
-  }
 };
 
 export default nextConfig;
