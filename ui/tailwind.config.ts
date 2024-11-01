2e354fb6a59f07f80f393323bfac3e93d810465b
Eunsoo Lee <eunsoogi.lee@gmail.com>
2024-11-01 20:32:57 +0900

    fix: layout tailwind

--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -1,16 +1,10 @@
-import flowbite from 'flowbite-react/tailwind'
-import type { Config } from 'tailwindcss'
+import flowbite from 'flowbite-react/tailwind';
+import flowbitePlugin from 'flowbite/plugin';
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   darkMode: 'class',
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-
-    // Flowbite content
-    flowbite.content()
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', flowbite.content()],
   theme: {
     extend: {
       boxShadow: {
@@ -19,34 +13,33 @@ const config: Config = {
         'dark-md': 'rgba(145, 158, 171, 0.3) 0px 0px 2px 0px, rgba(145, 158, 171, 0.02) 0px 12px 24px -4px',
         sm: '0 0.125rem 0.25rem rgba(0, 0, 0, 0.075)',
         'btn-shadow': 'box-shadow: rgba(0, 0, 0, .05) 0 9px 17.5px',
-        active: '0px 17px 20px -8px rgba(77,91,236,0.231372549)'
+        active: '0px 17px 20px -8px rgba(77,91,236,0.231372549)',
       },
       borderRadius: {
         sm: '7px',
         md: '9px',
         lg: '24px',
         tw: '12px',
-        page: '20px'
+        page: '20px',
       },
       container: {
         center: true,
-        padding: '30px'
+        padding: '30px',
       },
       gap: {
-        '30': '30px'
+        '30': '30px',
       },
       padding: {
-        '30': '30px'
+        '30': '30px',
       },
       margin: {
-        '30': '30px'
+        '30': '30px',
       },
-
       colors: {
         cyan: {
           '500': 'var(--color-primary)',
           '600': 'var(--color-primary)',
-          '700': 'var(--color-primary)'
+          '700': 'var(--color-primary)',
         },
         primary: 'var(--color-primary)',
         secondary: 'var(--color-secondary)',
@@ -56,20 +49,21 @@ const config: Config = {
         error: 'var(--color-error)',
         lightprimary: 'var(--color-lightprimary)',
         lightsecondary: 'var(--color-lightsecondary)',
-        lightsuccess: 'var( --color-lightsuccess)',
+        lightsuccess: 'var(--color-lightsuccess)',
         lighterror: 'var(--color-lighterror)',
         lightinfo: 'var(--color-lightinfo)',
         lightwarning: 'var(--color-lightwarning)',
         border: 'var(--color-border)',
         bordergray: 'var(--color-bordergray)',
-        lightgray: 'var( --color-lightgray)',
+        lightgray: 'var(--color-lightgray)',
         muted: 'var(--color-muted)',
         dark: 'var(--color-dark)',
         link: 'var(--color-link)',
-        darklink: 'var(--color-darklink)'
-      }
-    }
+        darklink: 'var(--color-darklink)',
+      },
+    },
   },
-  plugins: [require('flowbite/plugin')]
-}
-export default config
+  plugins: [flowbitePlugin],
+};
+
+export default config;
