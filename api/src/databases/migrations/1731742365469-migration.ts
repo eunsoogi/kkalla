2196bb083cf824e90583fcc0e056d57168108a58
Eunsoo Lee <eunsoogi.lee@gmail.com>
2024-11-16 18:46:57 +0900

    hotfix: migration

--- api/src/databases/migrations/1731742365469-migration.ts
+++ api/src/databases/migrations/1731742365469-migration.ts
@@ -1,73 +1,27 @@
-import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1731742365469 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.renameColumn(
-      'inference',
-      'rate',
-      new TableColumn({
-        name: 'order_ratio',
-        type: 'double',
-        default: 0,
-        isNullable: false,
-      }),
+    await queryRunner.query('ALTER TABLE `inference` CHANGE `rate` `order_ratio` DOUBLE NOT NULL DEFAULT 0');
+
+    await queryRunner.query(
+      'ALTER TABLE `inference` CHANGE `symbol_rate_lower` `weight_lower_bound` DOUBLE NOT NULL DEFAULT 0',
     );
 
-    await queryRunner.renameColumn(
-      'inference',
-      'symbol_rate_lower',
-      new TableColumn({
-        name: 'weight_lower_bound',
-        type: 'double',
-        default: 0,
-        isNullable: false,
-      }),
-    );
-
-    await queryRunner.renameColumn(
-      'inference',
-      'symbol_rate_upper',
-      new TableColumn({
-        name: 'weight_upper_bound',
-        type: 'double',
-        default: 0,
-        isNullable: false,
-      }),
+    await queryRunner.query(
+      'ALTER TABLE `inference` CHANGE `symbol_rate_upper` `weight_upper_bound` DOUBLE NOT NULL DEFAULT 0',
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.renameColumn(
-      'inference',
-      'order_ratio',
-      new TableColumn({
-        name: 'rate',
-        type: 'double',
-        default: 0,
-        isNullable: false,
-      }),
+    await queryRunner.query('ALTER TABLE `inference` CHANGE `order_ratio` `rate` DOUBLE NOT NULL DEFAULT 0');
+
+    await queryRunner.query(
+      'ALTER TABLE `inference` CHANGE `weight_lower_bound` `symbol_rate_lower` DOUBLE NOT NULL DEFAULT 0',
     );
 
-    await queryRunner.renameColumn(
-      'inference',
-      'weight_lower_bound',
-      new TableColumn({
-        name: 'symbol_rate_lower',
-        type: 'double',
-        default: 0,
-        isNullable: false,
-      }),
-    );
-
-    await queryRunner.renameColumn(
-      'inference',
-      'weight_upper_bound',
-      new TableColumn({
-        name: 'symbol_rate_upper',
-        type: 'double',
-        default: 0,
-        isNullable: false,
-      }),
+    await queryRunner.query(
+      'ALTER TABLE `inference` CHANGE `weight_upper_bound` `symbol_rate_upper` DOUBLE NOT NULL DEFAULT 0',
     );
   }
 }
