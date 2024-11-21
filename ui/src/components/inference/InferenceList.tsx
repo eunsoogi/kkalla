d3674f7d2ecf0c03a9fd5e20c67b74d907d531fe
Eunsoo Lee <eunsoogi.lee@gmail.com>
2024-11-21 13:35:40 +0900

    (#211) feat: copy link

--- ui/src/components/inference/InferenceList.tsx
+++ ui/src/components/inference/InferenceList.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import React, { Suspense } from 'react';
 
 import { useSuspenseQuery } from '@tanstack/react-query';
@@ -27,7 +28,9 @@ export const InferenceList: React.FC = () => {
 
   return (
     <div className='rounded-xl dark:shadow-dark-md shadow-md bg-white dark:bg-dark p-6 relative w-full min-h-full break-words'>
-      <h5 className='card-title text-dark dark:text-white mb-6'>{t('inference.list')}</h5>
+      <h5 className='card-title text-dark dark:text-white mb-6'>
+        <Link href='/inferences'>{t('inference.list')}</Link>
+      </h5>
       <div className='flex flex-col mt-2'>
         <Suspense fallback={<InferenceSkeleton />}>
           <InferenceListContent />
