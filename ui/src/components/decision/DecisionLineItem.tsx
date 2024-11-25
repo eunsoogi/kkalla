571d85bfb61e595e139f86663d1365e3033c2d49
Eunsoo Lee <eunsoogi.lee@gmail.com>
2024-11-25 18:23:30 +0900

    (#231) fix: missing question mark

--- ui/src/components/decision/DecisionLineItem.tsx
+++ ui/src/components/decision/DecisionLineItem.tsx
@@ -20,12 +20,12 @@ export const DecisionLineItem: React.FC<Decision> = (item: Decision) => {
             <p>{formatDate(new Date(item.createdAt || ''))}</p>
           </div>
           <div className='flex flex-col items-center'>
-            <div className={`rounded-full ${DECISION_STYLES[item.decision].dotStyle} p-1.5 w-fit`}></div>
+            <div className={`rounded-full ${DECISION_STYLES[item?.decision]?.dotStyle} p-1.5 w-fit`}></div>
             <div className='h-full w-px bg-border dark:bg-gray-800'></div>
           </div>
           <div className='flex gap-4'>
             <p className='text-dark text-start'>
-              <Badge className={DECISION_STYLES[item.decision].badgeStyle}>{item.decision}</Badge>
+              <Badge className={DECISION_STYLES[item?.decision]?.badgeStyle}>{item.decision}</Badge>
             </p>
             <p>{item.orderRatio * 100}%</p>
           </div>
