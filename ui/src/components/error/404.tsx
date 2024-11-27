4f1a7b6df858048e7e17fef249d5be5cbf52cfb5
Eunsoo Lee <22517955+eunsoogi@users.noreply.github.com>
2024-11-27 16:28:21 +0900

    (#248) hotfix: build

--- ui/src/components/error/404.tsx
+++ ui/src/components/error/404.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import React from 'react';
 import Link from 'next/link';
+import React from 'react';
 
 import { Button } from 'flowbite-react';
 import { useTranslations } from 'next-intl';
@@ -15,9 +15,7 @@ export const NotFoundError = () => {
       <h1 className='mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl'>
         {t('error.notFound.title')}
       </h1>
-      <p className='mt-6 text-base leading-7 text-gray-600 dark:text-gray-400'>
-        {t('error.notFound.description')}
-      </p>
+      <p className='mt-6 text-base leading-7 text-gray-600 dark:text-gray-400'>{t('error.notFound.description')}</p>
       <Button color='primary' as={Link} href='/' className='w-fit mt-6'>
         {t('error.notFound.home')}
       </Button>
--- ui/src/components/users/action.ts
+++ ui/src/components/users/action.ts
@@ -1,6 +1,7 @@
 'use server';
 
-import { PaginatedUsers } from '@/interfaces/user.interface';
+import { PaginatedItem } from '@/interfaces/item.interface';
+import { User } from '@/interfaces/user.interface';
 import { getClient } from '@/utils/api';
 
 export interface UsersParams {
@@ -8,14 +9,13 @@ export interface UsersParams {
   perPage?: number;
 }
 
-export const getUsersAction = async (params: UsersParams): Promise<PaginatedUsers> => {
+export const getUsersAction = async (params: UsersParams): Promise<PaginatedItem<User>> => {
   const client = await getClient();
 
   try {
     const { data } = await client.get('/api/v1/users', {
       params,
     });
-
     return data;
   } catch (error) {
     console.error('Failed to fetch users:', error);
