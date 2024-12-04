f94eca7fd0b55e5d82c3681c5dd78de008ca7552
Eunsoo Lee <22517955+eunsoogi@users.noreply.github.com>
2024-12-05 02:30:17 +0900

    (#271) fix: 운영 환경 로그 레벨 개선

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.NODE_ENV === 'production' ? ['error', 'warn', 'log'] : ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
 
   // Validation
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
