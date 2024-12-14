bf4915984b8ff369dcc8224b786280edcf954e15
Eunsoo Lee <eunsoogi.lee@gmail.com>
2024-12-14 19:07:26 +0900

    (#289) fix: calculate amount

--- api/src/modules/trade/trade.controller.ts
+++ api/src/modules/trade/trade.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 
 import { PaginatedItem } from '@/modules/item/item.interface';
 
@@ -6,6 +6,7 @@ import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { GoogleTokenAuthGuard } from '../auth/guards/google.guard';
 import { User } from '../user/entities/user.entity';
 import { GetTradeDto } from './dto/get-trade.dto';
+import { PostTradeDto } from './dto/post-trade.dto';
 import { ProfitDto } from './dto/profit.dto';
 import { Trade } from './entities/trade.entity';
 import { TradeService } from './trade.service';
@@ -21,6 +22,12 @@ export class TradeController {
   }
 
   @Post()
+  @UseGuards(GoogleTokenAuthGuard)
+  public async postTrade(@CurrentUser() user: User, @Body() body: PostTradeDto): Promise<Trade> {
+    return this.tradeService.postTrade(user, body);
+  }
+
+  @Post('request')
   @UseGuards(GoogleTokenAuthGuard)
   public async requestTrade(@CurrentUser() user: User): Promise<Trade[]> {
     return await this.tradeService.adjustPortfolios([user]);
