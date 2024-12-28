94785ca60721ade103f97d93dcf9465dee274a17
Eunsoo Lee <22517955+eunsoogi@users.noreply.github.com>
2024-12-28 10:43:05 +0900

    (#366) fix: 누락된 imports 추가

--- api/src/modules/trade/trade.module.ts
+++ api/src/modules/trade/trade.module.ts
@@ -3,13 +3,14 @@ import { Module } from '@nestjs/common';
 import { AccumulationModule } from '../accumulation/accumulation.module';
 import { InferenceModule } from '../inference/inference.module';
 import { NotifyModule } from '../notify/notify.module';
+import { ProfitModule } from '../profit/profit.module';
 import { SequenceModule } from '../sequence/sequence.module';
 import { UpbitModule } from '../upbit/upbit.module';
 import { TradeController } from './trade.controller';
 import { TradeService } from './trade.service';
 
 @Module({
-  imports: [SequenceModule, InferenceModule, AccumulationModule, UpbitModule, NotifyModule],
+  imports: [SequenceModule, InferenceModule, AccumulationModule, UpbitModule, ProfitModule, NotifyModule],
   controllers: [TradeController],
   providers: [TradeService],
   exports: [TradeService],
