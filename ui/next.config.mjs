976bcc7d2a79eb9ab557c254de2555f131a5c13c
Eunsoo Lee <eunsoogi.lee@gmail.com>
2024-12-28 15:45:26 +0900

    (#373) fix: 불필요한 디버그 로그 제거

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
   reactStrictMode: true,
   productionBrowserSourceMaps: false,
   output: 'standalone',
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   images: {
     remotePatterns: [
       {
