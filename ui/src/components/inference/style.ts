20ac88a5bf8e27d26e5c2ebb1ccac3b5452fcd37
Eunsoo Lee <eunsoogi.lee@gmail.com>
2025-01-04 11:34:33 +0900

    (#397) fix: lint

--- ui/src/components/inference/style.ts
+++ ui/src/components/inference/style.ts
@@ -1,7 +1,7 @@
 export const getRateColor = (rate: number) => {
   // rate를 0~1 범위로 정규화
   const normalizedRate = (rate + 1) / 2;
-  
+
   const red = Math.round(255 * (1 - normalizedRate));
   const green = Math.round(255 * normalizedRate);
 
