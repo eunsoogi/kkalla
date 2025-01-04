62b239f4bd3da66d898b15030c9b274732c750f1
Eunsoo Lee <eunsoogi.lee@gmail.com>
2025-01-04 11:48:47 +0900

    (#397) fix: 오타 수정

--- api/src/databases/migrations/1735957722577-migration.ts
+++ api/src/databases/migrations/1735957722577-migration.ts
@@ -4,7 +4,7 @@ export class Migration1735957722577 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     // rate 값을 0~1에서 -1~1로 스케일링
     await queryRunner.query(`
-      UPDATE trade
+      UPDATE inference
       SET rate = rate * 2 - 1
       WHERE rate >= 0 AND rate <= 1
     `);
@@ -13,7 +13,7 @@ export class Migration1735957722577 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<void> {
     // rate 값을 -1~1에서 0~1로 되돌리기
     await queryRunner.query(`
-      UPDATE trade
+      UPDATE inference
       SET rate = (rate + 1) / 2
       WHERE rate >= -1 AND rate <= 1
     `);
