1f47fb9dc56514c4b64880f087eccac9fbaaa9ec
Eunsoo Lee <22517955+eunsoogi@users.noreply.github.com>
2025-01-19 18:41:19 +0900

    (#431) fix: 복합 인덱스 구문 오류 수정

--- api/src/databases/migrations/1737274601607-migration.ts
+++ api/src/databases/migrations/1737274601607-migration.ts
@@ -1,11 +1,17 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, TableIndex } from 'typeorm';
 
 export class Migration1737274601607 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE INDEX "idx_notify_user_seq" ON "notify" ("user_id" ASC, "seq" DESC)`);
+    await queryRunner.createIndex(
+      'notify',
+      new TableIndex({
+        name: 'idx_notify_user_seq',
+        columnNames: ['user_id', 'seq'],
+      }),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "idx_notify_user_seq"`);
+    await queryRunner.dropIndex('notify', 'idx_notify_user_seq');
   }
 }
