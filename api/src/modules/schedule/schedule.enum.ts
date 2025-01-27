5c95c028b7ae69506e50ced4ed2f29a85fe1567a
Eunsoo Lee <eunsoogi.lee@gmail.com>
2025-01-27 14:04:50 +0900

    (#450) feat: 매도 수행을 15분 간격으로 변경

--- api/src/modules/schedule/schedule.enum.ts
+++ api/src/modules/schedule/schedule.enum.ts
@@ -1,4 +1,4 @@
 export enum ScheduleExpression {
   BUY_EVERY_4_HOURS = '0 0 */4 * * *',
-  SELL_EVERY_20_MINUTES = '0 */20 1-3,5-7,9-11,13-15,17-19,21-23 * * *',
+  SELL_EVERY_15_MINUTES = '0 */15 1-3,5-7,9-11,13-15,17-19,21-23 * * *',
 }
