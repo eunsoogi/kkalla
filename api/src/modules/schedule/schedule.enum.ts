12505c9a5a114b193f345409521a90de4ba78429
Eunsoo Lee <22517955+eunsoogi@users.noreply.github.com>
2025-02-08 08:02:22 +0900

    (#486) feat: 매매 시간을 매 30분으로 변경

--- api/src/modules/schedule/schedule.enum.ts
+++ api/src/modules/schedule/schedule.enum.ts
@@ -1,4 +1,4 @@
 export enum ScheduleExpression {
   EVERY_4_HOURS = '0 0 */4 * * *',
-  EVERY_15_MINUTES_WITHOUT_4_HOURS = '0 */15 1-3,5-7,9-11,13-15,17-19,21-23 * * *',
+  EVERY_30_MINUTES_WITHOUT_EVERY_4_HOURS = '0 */30 1-3,5-7,9-11,13-15,17-19,21-23 * * *',
 }
