bcc05ea643ab8b21fe332b141af0719abcd779bd
Eunsoo Lee <22517955+eunsoogi@users.noreply.github.com>
2025-02-28 08:01:57 +0900

    (#500) feat: 매일 6시마다 매집 종목을 구매하도록 변경

--- api/src/modules/schedule/schedule.enum.ts
+++ api/src/modules/schedule/schedule.enum.ts
@@ -1,4 +1,4 @@
 export enum ScheduleExpression {
-  EVERY_4_HOURS_WITH_NEW_ITEMS = '0 0 4,8 * * *',
+  EVERY_DAY_WITH_NEW_ITEMS = '0 0 6 * * *',
   EVERY_30_MINUTES_WITH_EXIST_ITEMS = '0 15,45 * * * *',
 }
