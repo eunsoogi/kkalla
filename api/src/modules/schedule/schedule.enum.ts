a1d68012bed289c5b233099c48a442bacebeff55
Eunsoo Lee <eunsoogi.lee@gmail.com>
2025-04-19 02:09:55 +0900

    (#520) fix: 2시간마다 추론 및 거래하도록 변경

--- api/src/modules/schedule/schedule.enum.ts
+++ api/src/modules/schedule/schedule.enum.ts
@@ -1,4 +1,4 @@
 export enum ScheduleExpression {
-  EVERY_DAY_WITH_NEW_ITEMS = '0 0 6,7 * * *',
-  EVERY_HOUR_WITH_EXIST_ITEMS = '0 35 * * * *',
+  EVERY_DAY_WITH_NEW_ITEMS = '0 35 6 * * *',
+  EVERY_HOUR_WITH_EXIST_ITEMS = '0 35 0,2,4,8,10,12,14,16,18,20,22 * * *',
 }
