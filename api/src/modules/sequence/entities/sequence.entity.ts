200ab0acdf1990a43d4d7d39985e0ed217679110
Eunsoo Lee <22517955+eunsoogi@users.noreply.github.com>
2025-09-03 08:16:56 +0900

    (#584) fix: Sequence 생성 큐 적용

--- api/src/modules/sequence/entities/sequence.entity.ts
+++ api/src/modules/sequence/entities/sequence.entity.ts
@@ -2,8 +2,6 @@ import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Sequence extends BaseEntity {
-  @PrimaryGeneratedColumn('increment', {
-    type: 'bigint',
-  })
+  @PrimaryGeneratedColumn('increment')
   value: number;
 }
--- api/src/modules/sequence/sequence.module.ts
+++ api/src/modules/sequence/sequence.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { Sequence } from './entities/sequence.entity';
 import { SequenceService } from './sequence.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Sequence])],
   providers: [SequenceService],
   exports: [SequenceService],
 })
