3639072ecb180f3c7718c920ec23fa4412298b18
Eunsoo Lee <eunsoogi.lee@gmail.com>
2025-09-02 22:49:37 +0900

    hotfix: Repository 의존성 추가

--- api/src/modules/sequence/sequence.module.ts
+++ api/src/modules/sequence/sequence.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Sequence } from './entities/sequence.entity';
 import { SequenceService } from './sequence.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Sequence])],
   providers: [SequenceService],
   exports: [SequenceService],
 })
