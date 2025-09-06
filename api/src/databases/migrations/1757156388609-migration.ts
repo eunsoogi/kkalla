d2c6adee8e02919d190303f63937c84525728360
Eunsoo Lee <eunsoogi.lee@gmail.com>
2025-09-07 00:08:05 +0900

    hotfix: batch_id default 추가

--- api/src/databases/migrations/1757156388609-migration.ts
+++ api/src/databases/migrations/1757156388609-migration.ts
@@ -6,23 +6,35 @@ export class Migration1757156388609 implements MigrationInterface {
 
     // ticker 컬럼을 symbol 컬럼으로 변경
     const oldTickerColumn = table.findColumnByName('ticker');
-    const newSymbolColumn = new TableColumn({
-      name: 'symbol',
-      type: 'varchar',
-      length: '255',
-      isNullable: false,
-    });
+    if (oldTickerColumn) {
+      const newSymbolColumn = new TableColumn({
+        name: 'symbol',
+        type: 'varchar',
+        length: '255',
+        isNullable: false,
+      });
 
-    await queryRunner.changeColumn('balance_recommendation', oldTickerColumn, newSymbolColumn);
+      await queryRunner.changeColumn('balance_recommendation', oldTickerColumn, newSymbolColumn);
+    }
 
     // batch_id 컬럼 추가
     const batchIdColumn = new TableColumn({
       name: 'batch_id',
       type: 'uuid',
       isNullable: false,
+      default: 'UUID()',
     });
 
     await queryRunner.addColumn('balance_recommendation', batchIdColumn);
+
+    // default 제거
+    const batchIdColumnWithoutDefault = new TableColumn({
+      name: 'batch_id',
+      type: 'uuid',
+      isNullable: false,
+    });
+
+    await queryRunner.changeColumn('balance_recommendation', batchIdColumn, batchIdColumnWithoutDefault);
 
     // batch_id와 symbol의 복합 유니크 인덱스 생성
     const uniqueIndex = new TableIndex({
@@ -35,8 +47,6 @@ export class Migration1757156388609 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    const table = await queryRunner.getTable('balance_recommendation');
-
     // 유니크 인덱스 삭제
     await queryRunner.dropIndex('balance_recommendation', 'idx_balance_recommendation_batch_id_symbol');
 
@@ -44,14 +54,18 @@ export class Migration1757156388609 implements MigrationInterface {
     await queryRunner.dropColumn('balance_recommendation', 'batch_id');
 
     // symbol 컬럼을 ticker 컬럼으로 변경
+    const table = await queryRunner.getTable('balance_recommendation');
     const oldSymbolColumn = table.findColumnByName('symbol');
-    const newTickerColumn = new TableColumn({
-      name: 'ticker',
-      type: 'varchar',
-      length: '255',
-      isNullable: false,
-    });
 
-    await queryRunner.changeColumn('balance_recommendation', oldSymbolColumn, newTickerColumn);
+    if (oldSymbolColumn) {
+      const newTickerColumn = new TableColumn({
+        name: 'ticker',
+        type: 'varchar',
+        length: '255',
+        isNullable: false,
+      });
+
+      await queryRunner.changeColumn('balance_recommendation', oldSymbolColumn, newTickerColumn);
+    }
   }
 }
